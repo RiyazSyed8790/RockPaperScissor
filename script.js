9a97bb1afe4fd8aec4985d011e596a90a0f54543
RiyazSyed8790 <riyazsyed.8790@gmail.com>
2021-06-18 15:45:16 +0530

    need some responsiveness done.

--- script.js
+++ script.js
@@ -3,53 +3,86 @@ const computerScore= document.getElementById('computer-score');
 const userScore= document.getElementById('user-score');
 let cScore = 0;
 let pScore= 0;
+
+function gameLogic(userInput,computerinput){
+    if(userInput===computerinput){
+        document.getElementById("result").innerText="Draw";
+        console.log("its a draw");
+        return;
+    }
+    if(userInput==='rock'){
+        if(computerinput==='paper'){
+            document.getElementById("result").innerText="You Lose";
+            console.log('You lose');
+            cScore++;
+            return;
+        }
+        else{
+            document.getElementById("result").innerText="You Win";
+            console.log('You win');
+            pScore++;   
+            return;
+        }
+    }
+    if(userInput==='paper'){
+       if(computerinput==='scissor'){
+        document.getElementById("result").innerText="You lose";
+           console.log('You lose');
+           cScore++;
+           return;
+       }
+       else{
+        document.getElementById("result").innerText="You Win";
+           console.log('You win');
+           pScore++;
+           return;
+       }
+   }
+   if(userInput==='scissor'){
+       if(computerinput==='rock'){
+           console.log('You lose');
+           cScore++;
+           return;
+       }
+       else{
+           console.log('You win');
+           pScore++;
+           return;
+       }
+   }
+}
 function check(a){
     let userInput = a ;
     var choices = ['rock' , 'paper' , 'scissor'];
-    var num = Math.trunc(Math.random()*3)
+    var num = Math.trunc(Math.random()*3);
     let computerinput = choices[num];
     document.getElementById("igi").classList.remove("hidden");
     /*document.getElementById("user choice").innerText = "You chose:" + userInput ;
-    document.getElementById("computer-choice").innerText = "Computer chose:" + computerinput ;
-    */gameLogic(userInput,computerinput)
+    document.getElementById("computer-choice").innerText = "Computer chose:" + computerinput ;*/
+    
+    
+    document.getElementById("user choice").innerText= "You choose:" + userInput;
+    document.getElementById("computer-choice").innerText="Computer choose:" + computerinput ;
+    gameLogic(userInput,computerinput);
+    computerScore.innerText=cScore;
+    userScore.innerText=pScore;
+    if(cScore==10 || pScore==10){
+        if(cScore==10){
+            alert("The machine won!");
+            
+            return;
+        }
+        if(pScore==10){
+            alert("You won!");
+            return;
+        }
+    }
+    /*else{
+        alert("Game Over! You Won!");
+    }*/
     
     }
- function gameLogic(userInput,computerinput){
-     if(userInput===computerinput){
-         console.log("its a draw");
-         return;
-     }
-     if(userInput=='rock'){
-         if(computerinput=='paper'){
-             console.log('You lose');
-             return;
-         }
-         else{
-             console.log('You win');
-             return;
-         }
-     }
-     if(userInput=='paper'){
-        if(computerinput=='scissor'){
-            console.log('You lose');
-            return;
-        }
-        else{
-            console.log('You win');
-            return;
-        }
-    }
-    if(userInput=='scissor'){
-        if(computerinput=='rock'){
-            console.log('You lose');
-            return;
-        }
-        else{
-            console.log('You win');
-            return;
-        }
-    }
- }
+ 
     
  function gamestart(){
     
